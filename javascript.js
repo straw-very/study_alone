7cf1866cdd34975b85f662c54cbd62cdc4f825e0
Bananasareoriginallywhite <128679450+Bananasareoriginallywhite@users.noreply.github.com>
2024-07-31 14:41:49 +0000

    ff

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const username=document.getElementById("username").value;
+
+
+
+
+    
+
+
+})
